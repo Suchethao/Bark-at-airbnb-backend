fd7e803eabb5933d7b9db0169a2443b6c593a557
suchethaleonidou <Suchethao>
2023-03-29 21:40:33 -0400

    fixed schema types

--- models/DogParkSchema.js
+++ models/DogParkSchema.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose'
 
 // Define the schema for houses, including the subdocument for characters
 const dogparkSchema = new mongoose.Schema({
-    Name: String,
-    neighborhood: String,
-    Address: String,
-    DogRuns_Type: String,
-    Accessible: String,
+    "Prop_ID": String,
+    "Name": String,
+    "neighborhood": String,
+    "Address": String,
+    "DogRuns_Type": String,
+    "Accessible": String,
+    "Notes": String,
     });
 
 export default mongoose.model('Dogpark', dogparkSchema)
