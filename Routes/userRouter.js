4df480466c52831db9dc7bf6c215b8287474df37
Taylor Weirtz <teweirtz@gmail.com>
2023-03-29 22:39:10 -0400

    realized things weren't working because the backend didn't know what to check for in the login request if the user isn't registered into the database so I wrote a register function

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import user from "../controllers/userController.js";
+import userController from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,34 @@ import {jwtSecret, jwtSession} from '../config/config.js';
 import User from '../models/userSchema.js';
 
 const userController = {
-login: (req, res) => {
+  register: (req, res) => {
+    if (req.body.email && req.body.password) {
+      let newUser = {
+        email: req.body.email,
+        password: req.body.password,
+      };
+      User.findOne({email: req.body.email}).then(user) => {
+        if (user) {
+          User.createConnection(newUser)
+          .then((user) => {
+            if (user) {
+              var payload = {
+                id: newUser.id,
+              };
+              var token = jwt.encode(payload, jwtSecret);
+              res.json({
+                token: token,
+              });
+            } else {
+              console.log("failed to create account");
+              res.sendStatus(401);
+            }
+          });
+        }
+      }
+    }
+  },
+  login: (req, res) => {
   if (req.body.email && req.body.password) {
     User.findOne({email: req.body.email}).then(user) => {
       if (user) {
@@ -30,5 +57,5 @@ login: (req, res) => {
   }
 }
     
-export default userController
+export default userController;
 
